ed6ee213b7e5902c67664a4dd813a6087edf7a15
zhkl0228 <zhkl0228@qq.com>
2023-04-06 15:34:47 +0800

    Connect vpn success.

--- InspectorVpn/ViewController.m
+++ InspectorVpn/ViewController.m
@@ -18,24 +18,120 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    vpnManager = [NETunnelProviderManager new];
-    NSLog(@"viewDidLoad vpnManager=%@", vpnManager);
-    
-    NSNotificationCenter *center = [NSNotificationCenter defaultCenter];
-    [center addObserver: self selector:@selector(vpnStatusDidChanged:) name:NEVPNStatusDidChangeNotification object:nil];
+    [NETunnelProviderManager loadAllFromPreferencesWithCompletionHandler:^(NSArray<NETunnelProviderManager *> * _Nullable managers, NSError * _Nullable error) {
+        self->vpnManager = [managers firstObject];
+        
+        if(self->vpnManager) {
+            [self vpnStatusDidChanged: nil];
+        }
+        
+        NSNotificationCenter *center = [NSNotificationCenter defaultCenter];
+        [center addObserver: self selector:@selector(vpnStatusDidChanged:) name:NEVPNStatusDidChangeNotification object:nil];
+        
+        NSLog(@"viewDidLoad vpnManager=%@, error=%@", self->vpnManager, error);
+    }];
 }
 
 - (void) vpnStatusDidChanged: (NSNotification *) notification {
     NEVPNStatus status = [[vpnManager connection] status];
-    NSLog(@"vpnStatusDidChanged notification=%@, status=%ld", notification, (long)status);
-}
+    switch (status) {
+        case NEVPNStatusInvalid:
+            [self.toggle setEnabled: YES];
+            [self.toggle setOn: NO];
 
-- (void) setValue:(id)value forUndefinedKey:(NSString *)key {
-    NSLog(@"setUndefinedValue key=%@, value=%@", key, value);
+            [self.spinner stopAnimating];
+            [self.statusLabel setText: @"Invalid"];
+            break;
+        case NEVPNStatusDisconnected:
+            [self.toggle setEnabled: YES];
+            [self.toggle setOn: NO];
+
+            [self.spinner stopAnimating];
+            [self.statusLabel setText: @"Disconnected"];
+            break;
+        case NEVPNStatusConnecting:
+            [self.toggle setEnabled: NO];
+            [self.toggle setOn: YES];
+
+            [self.spinner startAnimating];
+            [self.statusLabel setText: @"Connecting..."];
+            break;
+        case NEVPNStatusConnected:
+            [self.toggle setEnabled: YES];
+            [self.toggle setOn: YES];
+
+            [self.spinner stopAnimating];
+            [self.statusLabel setText: @"Connected"];
+            break;
+        case NEVPNStatusDisconnecting:
+            [self.toggle setEnabled: NO];
+            
+            [self.spinner startAnimating];
+            [self.statusLabel setText: @"Disconnecting..."];
+            break;
+        default:
+            NSLog(@"vpnStatusDidChanged notification=%@, status=%ld", notification, (long)status);
+            break;
+    }
 }
 
 - (void) toggle: (UISwitch *) sender {
-    NSLog(@"toggle sender=%@", sender);
+    if([sender isOn]) {
+        [self installProfile];
+    } else {
+        [[vpnManager connection] stopVPNTunnel];
+    }
+}
+
+-(void) startVpn: (BOOL) startImmediately {
+    NETunnelProviderProtocol *tunnelProtocol = [NETunnelProviderProtocol new];
+    [tunnelProtocol setServerAddress: @"localhost"];
+    [tunnelProtocol setProviderBundleIdentifier: @"com.github.zhkl0228.inspector.vpn.extension"];
+    [tunnelProtocol setDisconnectOnSleep: NO];
+    if (@available(iOS 14.2, *)) {
+        [tunnelProtocol setExcludeLocalNetworks: YES];
+    }
+    
+    [self->vpnManager setProtocolConfiguration: tunnelProtocol];
+    [self->vpnManager setLocalizedDescription: @"InspectorVpn"];
+    [self->vpnManager setEnabled: YES];
+    
+    if(startImmediately) {
+        NEVPNStatus status = [[self->vpnManager connection] status];
+        if(status) {
+            [self.toggle setOn: status == NEVPNStatusConnected];
+        }
+    }
+    
+    [self->vpnManager saveToPreferencesWithCompletionHandler:^(NSError * _Nullable error) {
+        NSLog(@"saveToPreferencesWithCompletionHandler error=%@", error);
+        if(error) {
+            return;
+        }
+        
+        if(startImmediately) {
+            BOOL started = [[self->vpnManager connection] startVPNTunnelAndReturnError:nil];
+            NSLog(@"saveToPreferencesWithCompletionHandler started=%d", started);
+        } else {
+            [self installProfile];
+        }
+    }];
+}
+
+-(void) installProfile {
+    [NETunnelProviderManager loadAllFromPreferencesWithCompletionHandler:^(NSArray<NETunnelProviderManager *> * _Nullable managers, NSError * _Nullable error) {
+        self->vpnManager = [managers firstObject];
+        NSLog(@"loadAllFromPreferencesWithCompletionHandler managers=%@, error=%@, manager=%@", managers, error, self->vpnManager);
+        if(error) {
+            return;
+        }
+        if(self->vpnManager) {
+            [self startVpn: YES];
+        } else {
+            self->vpnManager = [NETunnelProviderManager new];
+            [self startVpn: NO];
+        }
+    }];
 }
 
 
--- InspectorVpnTunnel/PacketTunnelProvider.m
+++ InspectorVpnTunnel/PacketTunnelProvider.m
@@ -12,33 +12,33 @@
 - (void) startTunnelWithOptions:(NSDictionary<NSString *,NSObject *> *)options completionHandler:(void (^)(NSError * _Nullable))completionHandler {
     NSLog(@"startTunnelWithOptions=%@, handler=%@", options, completionHandler);
     
-    @autoreleasepool {
-        NSString *vpn4 = @"10.1.10.1";
-        NSString *subnetMask4 = @"255.255.255.0";
-        
-        NEPacketTunnelNetworkSettings *settings = [[NEPacketTunnelNetworkSettings alloc] initWithTunnelRemoteAddress: @"127.0.0.1"];
-        
-        NEIPv4Settings *ipv4Settings = [[NEIPv4Settings alloc] initWithAddresses:[NSArray arrayWithObject: vpn4] subnetMasks: [NSArray arrayWithObject: subnetMask4]];
-        NEIPv4Route *defaultRoute = [NEIPv4Route defaultRoute];
-        [ipv4Settings setIncludedRoutes: [NSArray arrayWithObject: defaultRoute]];
-        [ipv4Settings setExcludedRoutes: [NSArray arrayWithObjects:
-                                          [[NEIPv4Route alloc] initWithDestinationAddress:@"10.0.0.0" subnetMask:@"255.0.0.0"],
-                                          [[NEIPv4Route alloc] initWithDestinationAddress:@"127.0.0.0" subnetMask:@"255.0.0.0"],
-                                          [[NEIPv4Route alloc] initWithDestinationAddress:@"192.168.0.0" subnetMask:@"255.255.0.0"],
-                                          nil]];
-        [settings setIPv4Settings: ipv4Settings];
-        
-        NEDNSSettings *dnsSettings = [[NEDNSSettings alloc] initWithServers: [NSArray arrayWithObjects: @"8.8.8.8", @"8.8.4.4", nil]];
-        [dnsSettings setMatchDomains: [NSArray arrayWithObject: @""]];
-        [settings setDNSSettings: dnsSettings];
-        [self setTunnelNetworkSettings:settings completionHandler:^(NSError * _Nullable error) {
-            NSLog(@"setTunnelNetworkSettings error=%@", error);
-        }];
-    }
+    NSString *vpn4 = @"10.1.10.1";
+    NSString *subnetMask4 = @"255.255.255.0";
+    
+    NEPacketTunnelNetworkSettings *settings = [[NEPacketTunnelNetworkSettings alloc] initWithTunnelRemoteAddress: @"127.0.0.1"];
+    
+    NEIPv4Settings *ipv4Settings = [[NEIPv4Settings alloc] initWithAddresses:[NSArray arrayWithObject: vpn4] subnetMasks: [NSArray arrayWithObject: subnetMask4]];
+    NEIPv4Route *defaultRoute = [NEIPv4Route defaultRoute];
+    [ipv4Settings setIncludedRoutes: [NSArray arrayWithObject: defaultRoute]];
+    [ipv4Settings setExcludedRoutes: [NSArray arrayWithObjects:
+                                      [[NEIPv4Route alloc] initWithDestinationAddress:@"10.0.0.0" subnetMask:@"255.0.0.0"],
+                                      [[NEIPv4Route alloc] initWithDestinationAddress:@"127.0.0.0" subnetMask:@"255.0.0.0"],
+                                      [[NEIPv4Route alloc] initWithDestinationAddress:@"192.168.0.0" subnetMask:@"255.255.0.0"],
+                                      nil]];
+    [settings setIPv4Settings: ipv4Settings];
+    
+    NEDNSSettings *dnsSettings = [[NEDNSSettings alloc] initWithServers: [NSArray arrayWithObjects: @"8.8.8.8", @"8.8.4.4", nil]];
+    [dnsSettings setMatchDomains: [NSArray arrayWithObject: @""]];
+    [settings setDNSSettings: dnsSettings];
+    [self setTunnelNetworkSettings:settings completionHandler:^(NSError * _Nullable error) {
+        completionHandler(error);
+        NSLog(@"setTunnelNetworkSettings error=%@", error);
+    }];
 }
 
 - (void) stopTunnelWithReason:(NEProviderStopReason)reason completionHandler:(void (^)(void))completionHandler {
     NSLog(@"stopTunnelWithReason=%ld, handler=%@", (long)reason, completionHandler);
+    completionHandler();
 }
 
 @end
